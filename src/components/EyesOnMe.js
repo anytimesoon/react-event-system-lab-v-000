cff3a5bbe4f2795c8078564ee669b913cace1d54
Damien de Lartigue <damien@delartigue.co.uk>
2021-04-02 11:57:20 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 export default class EyesOnMe extends Component {
   
+  focusCallback = (e) => {
+    console.log('Good!')
+  }
+  
+  blurCallback = (e) => {
+    console.log()
+  }
   
   render(){
-    
+    return <button onFocus={this.focusCallback} onBlur={this.blurCallback} >Click me!</button>
   }
 }
