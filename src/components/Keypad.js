519f2a801675bf72e248c27426c90476a07b3d0a
Damien de Lartigue <damien@delartigue.co.uk>
2021-04-02 11:52:03 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class KeyPad extends Component {
   
   keyUpCallback = () => {
-      console.log('')
+      console.log('Entering password...')
   }
   
   render(){
